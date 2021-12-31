1e4e6659619d56a97ea8f1076cd319d5e7bf2abb
Kirill Maltsev <maltsevkirill@gmail.com>
2021-12-31 14:21:11 +0100

    Remove rollup-plugin-polyfill-node

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import babel from "vite-babel-plugin";
-import nodePolyfills from 'rollup-plugin-polyfill-node';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
-        nodePolyfills(),
         babel(),
     ],
 });
